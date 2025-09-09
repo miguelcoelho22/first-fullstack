9ebbd65978d0faac581712068bc2c70a4f82ddb9
miguel arthur coelho <miguelarthurmeumine@gmail.com>
2025-09-09 16:13:56 -0300

    m

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Logo from './componentes/Logo'
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-Header'>
+        <Logo></Logo>
+      </header>
+    </div>
+  );
+}
+
+export default App;
